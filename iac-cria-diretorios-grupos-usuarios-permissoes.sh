18cd5b3dce3be6d5f10fe806fa5d2bade8608f63
Bruno Mulin <bbmsp@yahoo.com>
2024-08-28 20:42:32 -0300

    Adicionando usuarios aos grupos.

--- iac-cria-diretorios-grupos-usuarios-permissoes.sh
+++ iac-cria-diretorios-grupos-usuarios-permissoes.sh
@@ -13,3 +13,17 @@ echo "Criando grupos de usuários.."
 groupadd GRP_ADM
 groupadd GRP_COMPRAS
 groupadd GRP_VENDAS
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VENDAS
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VENDAS
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VENDAS
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
