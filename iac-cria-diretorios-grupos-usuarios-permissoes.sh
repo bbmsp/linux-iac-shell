8378048b3669186303311bf87ec53b08ea59e540
Bruno Mulin <bbmsp@yahoo.com>
2024-08-28 20:47:57 -0300

    Adicionando owner e permissôes aos diretórios criados.

--- iac-cria-diretorios-grupos-usuarios-permissoes.sh
+++ iac-cria-diretorios-grupos-usuarios-permissoes.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-echo "Criando diretórios.."
+echo "Criando diretórios..."
 
 mkdir /adm
 mkdir /compras
 mkdir /vendas
 mkdir /publico
 
-echo "Criando grupos de usuários.."
+echo "Criando grupos de usuários..."
 
 groupadd GRP_ADM
 groupadd GRP_COMPRAS
 groupadd GRP_VENDAS
 
-echo "Criando usuários..."
+echo "Criando e adicionando usuários aos grupos..."
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
@@ -27,3 +27,16 @@ useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEND
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_COMPRAS
+
+echo "Setando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VENDAS /vendas
+chown root:GRP_COMPRAS /compras
+
+chmod 770 /adm
+chmod 770 /compras
+chmod 770 /vendas
+chmod 777 /publico
+
+echo "Script executado e finalizado com sucesso!"
